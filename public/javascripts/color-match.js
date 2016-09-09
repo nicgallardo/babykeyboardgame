639b4ac0edb8c750b88671ea3d5f4e095d77c2b4
Nicholas Gallardo <nic.gallardo@gmail.com>
2016-09-09 12:31:28 -0600

    anit color hit works

--- public/javascripts/color-match.js
+++ public/javascripts/color-match.js
@@ -33,6 +33,14 @@ var callTest = function(){
   return splicedVal[0];
 };
 
+var unbinder = function(){
+  var tmpColors = colors;
+  for (var i = 0; i < tmpColors.length; i++) {
+    var selector = "." + tmpColors[i].classValue;
+    $(selector).unbind( "click" );
+  };
+};
+
 var createQuiz = function(){
   var currentValue = callTest();
   console.log('currentValue : ',currentValue.color);
@@ -41,11 +49,27 @@ var createQuiz = function(){
     '<h1>Which block is ' + currentValue.color+ '?</h1>'
   );
   var selector = "." + currentValue.classValue;
+  unbinder();
+  createAnti();
   $(selector).click(function(){
     $(selector).remove();
     createQuiz();
   });
-}
+};
+
+var createAnti = function(){
+  var antiClicks = colorState;
+  for (var i = 0; i < antiClicks.length; i++) {
+    var selector = "." + antiClicks[i].classValue;
+    $(selector).click(function(){
+      console.log('hit anit');
+    });
+  }
+  // $(selector).click(function(){
+  //   $(selector).remove();
+  //   createQuiz();
+  // });
+};
 
 $(document).ready(function(){
   for (var i = 0; i < colors.length; i++) {
